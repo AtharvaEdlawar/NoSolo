8bdc713b72cfcf6af120eb5bfefaa43551ae5e88
Atharva Edlawar <atharva89edlawar@gmail.com>
2024-09-28 17:07:07 +0530

    hopefull this shit will work

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  base: './',  
   server:{
     proxy: {
       '/api': 'http://localhost:4000',
