e7f90699776f37c7d3a40257d9b82b47087f1e8a
Atharva Edlawar <atharva89edlawar@gmail.com>
2024-10-24 17:45:24 +0530

    asdwasd

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 const corsOptions = {
-    origin: '*',
+    origin: 'https://no-solo.vercel.app/',
     credentials: true,
     optionSuccessStatus: 200,
 };
