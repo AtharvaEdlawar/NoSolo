607f12727d4e58346c8ba546766a23e769d0bffa
Atharva Edlawar <atharva89edlawar@gmail.com>
2024-10-24 20:44:33 +0530

    hello

--- src/components/pages/cosplayers.jsx
+++ src/components/pages/cosplayers.jsx
@@ -21,7 +21,7 @@ export default function Cosplayers() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    axios.post(`./api/submit`, formData, {
+    axios.post(`https://no-solo.vercel.app/api/submit`, formData, {
       headers: {
         'Content-Type': 'application/json',
       },
