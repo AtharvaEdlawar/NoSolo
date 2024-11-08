943797eac48ad5fd738b11db08a501aa25622dec
Atharva Edlawar <atharva89edlawar@gmail.com>
2024-11-09 02:34:31 +0530

    changed to api call and the vercel.json

--- src/components/pages/Artists.jsx
+++ src/components/pages/Artists.jsx
@@ -25,7 +25,7 @@ export default function Artist() {
     e.preventDefault();
     
     // Directly send the artistData as a JSON object hahahahaasd
-    axios.post(`https://no-solo-ebda.vercel.app/api/submit`, artistData, {
+    axios.post(`https://no-solo-ebda.vercel.app/api/Artistsubmit`, artistData, {
         headers: {
           "Content-Type": 'application/json',
         },
