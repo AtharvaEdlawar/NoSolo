51968291bb0c99480b49df9f62634f2b730ad3a6
AtharvaEdlawar <91869081+AtharvaEdlawar@users.noreply.github.com>
2024-11-26 16:41:03 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try{
         const conn = await mongoose.connect(
-            'mongodb+srv://atharva89edlawar:GQqmt1jAHrvq3fiE@cluster0.fkdl9.mongodb.net/cosplayers,Artists?retryWrites=true&w=majority&appName=Cluster0');
+            'YOUR MONGODB ATLAS CLUSTOR');
         console.log(`MongoDB connected: ${conn.connection.host}`);
     }catch (error) {
         console.error(error);
